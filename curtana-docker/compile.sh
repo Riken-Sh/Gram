f29dab23c434ddc8c466dcbb2a8581b0d18fc879
Riken-Sh <yash.rajpt@proton.me>
2025-03-18 06:40:55 +0000

    DOCKER SUPPORT TEST

--- curtana-docker/compile.sh
+++ curtana-docker/compile.sh
@@ -5,6 +5,6 @@ PACKAGES_DIR="$HOME/droidian/packages"
 mkdir -p "$KERNEL_DIR"
 mkdir -p "$PACKAGES_DIR"
 cd "$KERNEL_DIR"
-git clone --depth 1 https://github.com/HarshitBhatt043/curtana.git
+git clone --depth 1 https://github.com/Riken-Sh/Gram
 mv -f curtana/{.,}* "$KERNEL_DIR"
 docker run --rm -v "$PACKAGES_DIR:/buildd" -v "$KERNEL_DIR:/buildd/sources" quay.io/droidian/build-essential:trixie-amd64 /bin/bash /buildd/sources/curtana-docker/kernel.sh
